fc6d80662698e4b7e8ff9c57e43e3e09350d828b
rawai61 <32534497+rawai61@users.noreply.github.com>
2017-10-16 17:14:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('BXaPBnJWb5J4qD5bQSkApsWIQTNS2fUgAdg4MyftueNW2mHKrq5laVLXRjAzGE/arhek9NU64myzM6r8kHyDZ3rFJsHSxy8sP+fVIwbb15br8i0lEwSA+CWxkl1v6WDPqwh4beMoyJ5nEIHCOEet+wdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['795f0abecb33a523dc346e84a3f4a0b2' => '795f0abecb33a523dc346e84a3f4a0b2']);
 $response = $bot->getProfile('<userId>');
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
