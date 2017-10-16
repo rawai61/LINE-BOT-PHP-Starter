6a37600cf1d5ad04f7e684178b2545ef2c996b07
rawai61 <32534497+rawai61@users.noreply.github.com>
2017-10-16 19:37:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -46,7 +46,7 @@ if (!is_null($events['events'])) {
 }
 echo "OK";
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('BXaPBnJWb5J4qD5bQSkApsWIQTNS2fUgAdg4MyftueNW2mHKrq5laVLXRjAzGE/arhek9NU64myzM6r8kHyDZ3rFJsHSxy8sP+fVIwbb15br8i0lEwSA+CWxkl1v6WDPqwh4beMoyJ5nEIHCOEet+wdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['795f0abecb33a523dc346e84a3f4a0b2' => '<channel secret>']);
 $response = $bot->getProfile('<userId>');
 if ($response->isSucceeded()) {
