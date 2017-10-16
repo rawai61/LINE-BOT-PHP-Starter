56c3217926d1aa6756d9d49b10def62aa0aab206
rawai61 <32534497+rawai61@users.noreply.github.com>
2017-10-16 16:37:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 $access_token = 'tILI2h91gdcdxXKaUpt/yfXgIXcmK0D20ExoP1ZRft04RYjWm0HeICcNbBxGAH27rhek9NU64myzM6r8kHyDZ3rFJsHSxy8sP+fVIwbb15ZNqeDXX2yUG6tUYzEUehJytvpzvGzYe32Rdq9m8exkywdB04t89/1O/w1cDnyilFU=';
-
+GET https://api.line.me/v2/bot/profile/{userId}
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
 $response = $bot->getProfile('<userId>');
