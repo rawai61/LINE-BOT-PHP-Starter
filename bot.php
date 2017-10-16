c4c526a80d970bfcabe1a084daec447361391c9b
rawai61 <32534497+rawai61@users.noreply.github.com>
2017-10-16 19:24:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,18 +1,15 @@
-{
-	"events" : [
-		{
-			"type" : "message",
-			"replyToken" : "7e97cf1ec9......dcffd850",
-			"source" : {
-				"userId" : "U39f72cc028......8d460d6ddf",
-				"type" : "user"
-			},
-			"timestamp" : 1486930709120,
-			"message" : {
-				"type" : "text",
-				"id" : "5638..645..8",
-				"text" : "dddd"
-			}
-		}
-	]
-}
+<?php
+$access_token = 'BXaPBnJWb5J4qD5bQSkApsWIQTNS2fUgAdg4MyftueNW2mHKrq5laVLXRjAzGE/arhek9NU64myzM6r8kHyDZ3rFJsHSxy8sP+fVIwbb15br8i0lEwSA+CWxkl1v6WDPqwh4beMoyJ5nEIHCOEet+wdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
