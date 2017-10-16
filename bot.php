0a39c12f0a788096c931ffa0f69c87933bbb6761
rawai61 <32534497+rawai61@users.noreply.github.com>
2017-10-16 17:15:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,9 +1,18 @@
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('BXaPBnJWb5J4qD5bQSkApsWIQTNS2fUgAdg4MyftueNW2mHKrq5laVLXRjAzGE/arhek9NU64myzM6r8kHyDZ3rFJsHSxy8sP+fVIwbb15br8i0lEwSA+CWxkl1v6WDPqwh4beMoyJ5nEIHCOEet+wdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['795f0abecb33a523dc346e84a3f4a0b2' => '795f0abecb33a523dc346e84a3f4a0b2']);
-$response = $bot->getProfile('<userId>');
-if ($response->isSucceeded()) {
-    $profile = $response->getJSONDecodedBody();
-    echo $profile['displayName'];
-    echo $profile['pictureUrl'];
-    echo $profile['statusMessage'];
+{
+	"events" : [
+		{
+			"type" : "message",
+			"replyToken" : "7e97cf1ec9......dcffd850",
+			"source" : {
+				"userId" : "U39f72cc028......8d460d6ddf",
+				"type" : "user"
+			},
+			"timestamp" : 1486930709120,
+			"message" : {
+				"type" : "text",
+				"id" : "5638..645..8",
+				"text" : "dddd"
+			}
+		}
+	]
 }
